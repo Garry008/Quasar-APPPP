51f4c09e64cf38cd4c8950da994e02c1e49f68cf
Evgen <e.kurii@istu.edu.ua>
2024-09-07 10:01:13 +0300

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-page>
+<MainCarousel /> 
+      </q-page>
 </template>
 
 <script setup>
+import MainCarousel from "components/MainCarousel.vue";
 defineOptions({
   name: 'IndexPage'
 });
