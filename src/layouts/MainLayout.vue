c238f444e581086e8e92a3d1573736d7127a1d20
John Doe <ackold27@gmail.com>
2024-09-14 16:49:18 +0300

    first commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,25 +11,15 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink
           v-for="link in linksList"
@@ -46,25 +36,31 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Main",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "/",
   },
-  ]
+  {
+    title: "About",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "/about",
+  },
+];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="q-pa-md">
+    <q-card class="my-card" flat bordered>
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+      <q-list>
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="primary" name="local_bar" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Bar XYZ</q-item-label>
+            <q-item-label caption>Have a drink.</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="red" name="local_gas_station" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Gas Station</q-item-label>
+            <q-item-label caption>Fill your gas tank.</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="amber" name="local_movies" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Cinema XYZ</q-item-label>
+            <q-item-label caption>Watch a movie.</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 500px
+</style>
