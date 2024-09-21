7f702c5af4789588f27560eee3bb74c5d94827f6
Garry <e.SHCHERBAK@istu.edu.ua>
2024-09-21 10:10:20 +0300

    add time

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,15 +2,15 @@
   <q-page>
     <div class="q-pa-md q-gutter-sm">
       <q-bar dense class="bg-green text-white">
-        <div>Vodafon</div>
+        <div>Vodafone</div>
         <q-icon name="email" />
         <q-space />
         <q-icon name="bluetooth" />
         <q-icon name="signal_wifi_8_bar" />
         <q-icon name="signal_cellular_4_bar" />
-        <div class="gt-xs">99%</div>
+        <div class="gt-xs">{{ getRandomIntInclusive(0, 100) }}%</div>
         <q-icon name="battery_full" />
-        <div>08:48AM</div>
+        <div>{{ hour }}:{{ minutes }}:{{ seconds }}</div>
       </q-bar>
     </div>
     <MainCarousel />
@@ -18,8 +18,32 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import MainCarousel from "components/MainCarousel.vue";
 defineOptions({
   name: "IndexPage",
 });
+
+let hour = ref(0);
+let minutes = ref(0);
+let seconds = ref(0);
+
+function getTime() {
+  const date = new Date();
+  hour.value = date.getHours();
+  minutes.value = date.getMinutes();
+  seconds.value = date.getSeconds();
+}
+
+getTime();
+
+setInterval(function () {
+  getTime();
+}, 1000);
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
 </script>
