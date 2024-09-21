9bdc752884c80f22fa43b9e0d77dbd4e6aa9ac32
John Doe <ackold27@gmail.com>
2024-09-21 09:09:07 +0300

    add qbar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,25 @@
 <template>
   <q-page>
-<MainCarousel /> 
-      </q-page>
+    <div class="q-pa-md q-gutter-sm">
+      <q-bar dense class="bg-green text-white">
+        <div>Vodafon</div>
+        <q-icon name="email" />
+        <q-space />
+        <q-icon name="bluetooth" />
+        <q-icon name="signal_wifi_8_bar" />
+        <q-icon name="signal_cellular_4_bar" />
+        <div class="gt-xs">99%</div>
+        <q-icon name="battery_full" />
+        <div>08:48AM</div>
+      </q-bar>
+    </div>
+    <MainCarousel />
+  </q-page>
 </template>
 
 <script setup>
 import MainCarousel from "components/MainCarousel.vue";
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
